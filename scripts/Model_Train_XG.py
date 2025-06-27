cc713756ef62237ad09ad8f8a17bc1aedbbe5592
Nevan <nevanumar566@gmail.com>
2025-06-27 23:41:16 +0800

    Updated model

--- scripts/Model_Train_XG.py
+++ scripts/Model_Train_XG.py
@@ -9,7 +9,7 @@ import joblib
 
 #Load variables, split dataset
 X,y = joblib.load("models/train_ready_data.pkl")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
 
 #Balance the dataset
 sm = SMOTE(random_state=42)
